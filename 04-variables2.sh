cc15385281abc140ce2dfb9337f30a53b82baf65
Sai Satwik <saisatwik175@gmail.com>
2025-05-17 18:32:40 +0530

    4th file added

---
+++ 04-variables2.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+echo "variables that are not defined and passing while executing the script"
+
+person1=$1
+person2=$2
+person3=$3
+
+echo "$person1 and $person3 are friends"
+echo "$person2 and $person3 are best friends"
