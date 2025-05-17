476791d1069c8031f348f066e974bec1cb80fc73
Sai Satwik <saisatwik175@gmail.com>
2025-05-17 18:36:43 +0530

    5th file added

---
+++ 05-var-sensitive.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+echo "it shows hidden fields in variables how we entering passwords and taking input from user "
+echo "enter your name"
+
+read -s name
+
+echo " the name you entered is $name "
