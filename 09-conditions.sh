59ae82b297c9f91fcc213b0d26c92174e95ddcf3
Sai Satwik <saisatwik175@gmail.com>
2025-05-18 14:45:24 +0530

    9th file added

---
+++ 09-conditions.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+echo " conditions "
+echo " enter 1st number "
+read num1
+echo "enter 2nd number "
+read num2
+
+if[ $num1 -gt $num2 ]
+then
+    echo " $num1 is greater than $num2 "
+else
+    echo " $num2 is greater than $num1 "
+fi
